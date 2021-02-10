d3776e6e6c3d55a03d72217b03725143fdf547e7
lenny.garnier <lenny.garnier@epitech.eu>
2021-02-10 11:00:41 +0400

    frontend ws - Hello World

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <div id="app">
+    <HeaderBar></HeaderBar>
     <router-view/>
   </div>
 </template>
+
+<script>
+  import HeaderBar from './components/HeaderBar'
+  export default {
+    components: { HeaderBar }
+  }
+</script>
 
 <style lang="scss">
 #app {
---
+++ front/src/components/HeaderBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <html>
+            <p>Hello World</p>
+        </html>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+
+</style>
